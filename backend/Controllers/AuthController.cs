1c1bc1064eb0446ad7123c1ed8f1b6d83135aa24
Sarah Ait aider <sarah.ait-aider@etu.unistra.fr>
2026-04-21 16:14:52 +0200

    ajout du contrôleur d'authentification

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using backend.Interfaces;
+using backend.DTOs;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponseDto>> Login(LoginRequestDto loginRequestDto)
+        {
+            var authResponse = await _authService.LoginAsync(loginRequestDto);
+
+            if (authResponse == null)
+                return Unauthorized(new { message = "Email ou mot de passe incorrect." });
+
+            return Ok(authResponse);
+        }
+    }
+}
